da27520a38731d2b20cd82731c501be266c32628
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-03-02 19:30:15 +0100

    finish up

--- build_zip.sh
+++ build_zip.sh
@@ -2,7 +2,7 @@
 R -e 'library(rmarkdown); render("intro.Rmd")'
 
 # Zip all files
-zip -r ulyssis.zip zip intro.Rmd intro.pdf r_language.Rmd movies/
+zip -r ulyssis.zip zip intro.Rmd intro.pdf r_language.Rmd movies/movies.Rmd movies/omdb.db movies/rotten_tomatoes.csv
 
 # Put files on S3
 s3cmd put --acl-public ulyssis.zip s3://documents.datacamp.com/ulyssis/
