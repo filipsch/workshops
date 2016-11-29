b2d1805abccbcb7a4338a6460066745d4d66d96d
Filip Schouwenaars <filip@datacamp.com>
2016-11-29 17:17:51 +0100

    update script

--- build_zip.sh
+++ build_zip.sh
@@ -2,7 +2,7 @@
 tit=workshop
 
 # Zip all files
-zip -r $tit.zip introduction.Rmd
+zip -r $tit.zip introduction.Rmd introduction.html intro_r cs_movies intro_python cs_calhousing
 
 # Put files on S3
 aws s3 cp --acl public-read $tit.zip s3://documents.datacamp.com/
