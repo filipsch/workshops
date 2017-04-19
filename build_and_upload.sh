1d9da1b84c70fbd482f3b43c7a14a464f122d8cc
Filip Schouwenaars <filip@datacamp.com>
2017-04-19 09:30:25 +0200

    last tweaks and improvements

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -1,7 +1,10 @@
 # Convert intro.Rmd to PDF
 tit=sagioworkshop
 
-R -e 'rmarkdown::render("introduction.Rmd"); rmarkdown::render(input = "intro_r/r_language.Rmd"); knitr::purl(input = "intro_r/r_language.Rmd", output = "intro_r/r_language.R")'
+R -e 'rmarkdown::render("introduction.Rmd")'
+R -e 'rmarkdown::render(input = "intro_r/r_language.Rmd"); knitr::purl(input = "intro_r/r_language.Rmd", output = "intro_r/r_language.R")'
+R -e 'rmarkdown::render(input = "cs_bank/cs_bank.Rmd")'
+R -e 'rmarkdown::render(input = "cs_movies/cs_movies.Rmd")'
 
 # Zip all files
 zip -r $tit.zip introduction.Rmd introduction.html intro_r cs_movies cs_bank
