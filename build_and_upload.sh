87fb900e8f878ae7363c670f48f5274f29bf4330
Filip Schouwenaars <filip@datacamp.com>
2017-04-19 12:00:06 +0200

    update folder structure

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -2,12 +2,12 @@
 tit=sagioworkshop
 
 R -e 'rmarkdown::render("introduction.Rmd")'
-R -e 'rmarkdown::render(input = "intro_r/r_language.Rmd"); knitr::purl(input = "intro_r/r_language.Rmd", output = "intro_r/r_language.R")'
+R -e 'rmarkdown::render(input = "r_language/r_language.Rmd"); knitr::purl(input = "r_language/r_language.Rmd", output = "r_language/r_language.R")'
 R -e 'rmarkdown::render(input = "cs_bank/cs_bank.Rmd")'
 R -e 'rmarkdown::render(input = "cs_movies/cs_movies.Rmd")'
 
 # Zip all files
-zip -r $tit.zip introduction.Rmd introduction.html intro_r cs_movies cs_bank
+zip -r $tit.zip introduction.Rmd introduction.html r_language cs_movies cs_bank
 
 # Put files on S3
 aws s3 cp --acl public-read $tit.zip s3://documents.datacamp.com/
